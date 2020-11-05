be1a2d170b897cce914011f6ba81bebc60060642
akbariyansyah <riyansean21366@gmail.com>
2020-11-05 17:06:41 +0700

    -added bottom nav bar

--- app/src/main/java/com/example/navigation_component/MainActivity.kt
+++ app/src/main/java/com/example/navigation_component/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -16,6 +17,7 @@ import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +26,12 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.findNavController()
 
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment,R.id.searchFragment)
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottom_nav.setupWithNavController(navController)
 
     }
 
---
+++ app/src/main/java/com/example/navigation_component/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navigation_component
+
+import androidx.fragment.app.Fragment
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+}
